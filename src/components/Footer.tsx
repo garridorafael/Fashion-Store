2075438831cf419e9d8adddf2071cc2f009f3e7c
Rafael <rgarrido846@gmail.com>
2023-07-04 12:42:12 -0300

    feat: add footer component and fonts

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <footer className="w-screen bg-black h-[156px] fixed bottom-0">
+      <div className="flex justify-center items-center h-full">
+        <h3 className="text-white text-center text-[19px] font-bold">
+          Todos os direitos reservados - Kenzie Academy Brasil
+        </h3>
+      </div>
+    </footer>
+  );
+}
