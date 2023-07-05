4a27e1ddd7298583224bfa0e0f626723b207de46
imgustacabral <imgustacabral@gmail.com>
2023-07-05 07:20:56 -0300

    refactor: change assets directory and Cart products rendering

--- src/components/Cart/CartModalCard.tsx
+++ src/components/Cart/CartModalCard.tsx
@@ -1,29 +1,35 @@
-import removeImg from '../../../assets/Vector (1).svg';
+import removeImg from '../../assets/remove-from-cart.svg';
 
 // eslint-disable-next-line import/no-cycle
 import { IProduct } from './CartModal';
 
-interface IProductListProps {
-  products: IProduct[],
+interface CartModalCardProps {
+  product: IProduct;
 }
 
-export function CartModalCard({ products } : IProductListProps) {
+export function CartModalCard({ product }: CartModalCardProps) {
   return (
-    <ul>
-      {
-        products.map((product) => (
-          <li key={product.id} className="flex justify-between items-center">
-            <div>
-              <img src={product.image} alt="Imagem do produto" className="rounded-lg" />
-              <div className="flex flex-col justify-between">
-                <h3>{product.name}</h3>
-                <p>{(product.price).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</p>
-              </div>
-            </div>
-            <img src={removeImg} alt="Traço preto usado para remover produto do carrinho" />
-          </li>
-        ))
-      }
-    </ul>
+    <li className="flex justify-between items-center">
+      <div>
+        <img
+          src={product.image}
+          alt="Imagem do produto"
+          className="rounded-lg"
+        />
+        <div className="flex flex-col justify-between">
+          <h3>{product.name}</h3>
+          <p>
+            {product.price.toLocaleString('pt-br', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
+          </p>
+        </div>
+      </div>
+      <img
+        src={removeImg}
+        alt="Traço preto usado para remover produto do carrinho"
+      />
+    </li>
   );
 }
