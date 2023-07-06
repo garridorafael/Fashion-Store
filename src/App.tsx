844c2c2d50811c863c1c9382a5fbeb4fb4528cb9
Rafael <rgarrido846@gmail.com>
2023-07-06 09:52:37 -0300

    fix: syntax errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Paragraph } from './components/Paragraph';
+import { AdminPage } from './pages/Admin/AdminProductsPage';
 
 function App() {
-  return <Paragraph text="Hello World" />;
+  const [showAdminPage, setShowAdminPage] = useState(false);
+
+  const handleClick = () => {
+    setShowAdminPage(true);
+  };
+
+  return (
+    <>
+      <Paragraph text="Olá Mundo" />
+      <button onClick={handleClick}>Abrir página de administração</button>
+
+      {showAdminPage && <AdminPage />}
+    </>
+  );
 }
+
 export default App;
