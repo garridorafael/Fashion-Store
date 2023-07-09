f316b0f247694b1226e95b04b290fcf2df33684a
imgustacabral <imgustacabral@gmail.com>
2023-07-09 16:36:21 -0300

    fix: change route names

--- src/pages/Admin/AdminPage.tsx
+++ src/pages/Admin/AdminPage.tsx
@@ -1,24 +1,36 @@
 import { useNavigate } from 'react-router-dom';
 import { Heading } from '../../components/Heading';
-import logo from '../../assets/FASHIONSTORE.svg';
 import { Paragraph } from '../../components/Paragraph';
 import { Footer } from '../../components/Footer';
+import { Header } from '../../components/Header';
 
 export function AdminPage() {
   const navigate = useNavigate();
   return (
     <>
-      <header className="flex justify-center items-center my-[20px]">
-        <img src={logo} alt="Logo da Fashion Store - Escrito FASHION STORE em preto" />
-      </header>
+      <Header unprotected />
+
       <div className="container px-4 max-w-[1200px] mx-auto box-border">
         <nav className="flex gap-3 my-[30px]">
-          <button className="text-xl leading-8 font-heading uppercase font-medium" onClick={() => navigate('/admin')}>início</button>
-          <button className="text-xl leading-8 font-heading uppercase font-medium" onClick={() => navigate('/adminProducts')}>produtos</button>
+          <button
+            className="text-xl leading-8 font-heading uppercase font-medium"
+            onClick={() => navigate('/admin')}
+          >
+            início
+          </button>
+          <button
+            className="text-xl leading-8 font-heading uppercase font-medium"
+            onClick={() => navigate('/admin/products')}
+          >
+            produtos
+          </button>
         </nav>
         <div className="flex flex-col box-content">
           <Heading title="painel do administrador" />
-          <Paragraph text="Seja bem vindo, administrador!" className="my-[30px]" />
+          <Paragraph
+            text="Seja bem vindo, administrador!"
+            className="my-[30px]"
+          />
         </div>
       </div>
       <Footer className="fixed bottom-0" />
